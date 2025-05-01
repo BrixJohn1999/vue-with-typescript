b09487a40d67a73f2654a77e1673563acd2e2ac8
brix <brixjohnovesteras@gmail.com>
2025-05-02 01:09:36 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,25 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <h1>Welcome to Your Vue.js App</h1>
+    <p>
+      This is a simple Vue.js application that serves as a starting point for
+      your projects.
+    </p>
+    <p>
+      You can modify this template to suit your needs and start building your
+      application.
+    </p>
+    `
   </main>
 </template>
 
+<script lang="ts" setup>
+import { defineComponent } from "vue";
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+main {
+  max-width: 800px;
+  margin: 1rem auto;
 }
 </style>
